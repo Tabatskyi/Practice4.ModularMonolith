b3d1c8afb1d9ab558e6c29814dcf3f4dcde88946
Mark Tabatskyi <mark.tabatskyi@gmail.com>
2026-04-05 18:52:51 +0300

    integrated producer to core

--- src/CoreService/Api/Program.cs
+++ src/CoreService/Api/Program.cs
@@ -1,19 +1,30 @@
 using System.Text.Json.Serialization;
+using MassTransit;
 using MediatR;
 using Modules.Core.Application.API.Users;
 using Modules.Core.Application.Commands;
 using Modules.Core.Infrastructure;
 using Modules.Core.Infrastructure.Persistence;
+using Shared.Api;
+using Shared.Contracts.Events;
 using Shared.Migrations;
 
 namespace Api;
 
 public class Program
 {
+    private const string CoreItemCreatedEventName = "core-item.created";
+
     public static void Main(string[] args)
     {
 
         var builder = WebApplication.CreateBuilder(args);
+
+        var rabbitMqHost = Utils.ResolveRabbitMqSetting(builder.Configuration["RabbitMq:Host"], "RabbitMq__Host", "localhost");
+        var rabbitMqPort = Utils.ResolveRabbitMqPort(builder.Configuration["RabbitMq:Port"]);
+        var rabbitMqVirtualHost = Utils.ResolveRabbitMqSetting(builder.Configuration["RabbitMq:VirtualHost"], "RabbitMq__VirtualHost", "/");
+        var rabbitMqUsername = Utils.ResolveRabbitMqSetting(builder.Configuration["RabbitMq:Username"], "RabbitMq__Username", "guest");
+        var rabbitMqPassword = Utils.ResolveRabbitMqSetting(builder.Configuration["RabbitMq:Password"], "RabbitMq__Password", "guest");
 
         builder.Services.ConfigureHttpJsonOptions(options =>
         {
@@ -25,6 +36,22 @@ public class Program
 
         builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateListingCommand).Assembly));
         builder.Services.AddCoreInfrastructure(builder.Configuration);
+        builder.Services.AddMassTransit(configurator =>
+        {
+            configurator.UsingRabbitMq((_, cfg) =>
+            {
+                cfg.Host(rabbitMqHost, rabbitMqPort, rabbitMqVirtualHost, hostConfigurator =>
+                {
+                    hostConfigurator.Username(rabbitMqUsername);
+                    hostConfigurator.Password(rabbitMqPassword);
+                });
+
+                cfg.Message<CoreItemCreatedEvent>(topology =>
+                {
+                    topology.SetEntityName(CoreItemCreatedEventName);
+                });
+            });
+        });
 
         var app = builder.Build();
 
@@ -45,11 +72,34 @@ public class Program
             app.MapGet("/", () => Results.Redirect("/swagger"));
         }
 
-        app.MapPost("/core-items", async (CreateListingRequest request, ISender sender, CancellationToken cancellationToken) =>
+        app.MapPost("/core-items", async (CreateListingRequest request, ISender sender, IPublishEndpoint publishEndpoint, HttpContext httpContext, CancellationToken cancellationToken) =>
         {
             try
             {
                 var id = await sender.Send(new CreateListingCommand(request.Title, request.Price, request.OwnerUserId),cancellationToken);
+
+                var correlationIdHeaderValue = httpContext.Request.Headers.TryGetValue(Utils.CorrelationIdHeaderName, out var headerValue)
+                    ? headerValue.ToString()
+                    : null;
+                var correlationId = Utils.ResolveCorrelationId(correlationIdHeaderValue, httpContext.TraceIdentifier);
+                var coreItemCreatedEvent = new CoreItemCreatedEvent(
+                    EventId: Guid.NewGuid(),
+                    OccurredAt: DateTimeOffset.UtcNow,
+                    CorrelationId: correlationId,
+                    CoreItemId: id,
+                    OwnerUserId: request.OwnerUserId,
+                    Summary: request.Title);
+
+                await publishEndpoint.Publish(coreItemCreatedEvent, publishContext =>
+                {
+                    publishContext.SetRoutingKey(CoreItemCreatedEventName);
+
+                    if (Guid.TryParse(correlationId, out var parsedCorrelationId))
+                    {
+                        publishContext.CorrelationId = parsedCorrelationId;
+                    }
+                }, cancellationToken);
+
                 var listing = await sender.Send(new GetListingByIdCommand(id), cancellationToken);
 
                 return listing is null
